53a5227af71552e6fea6c73f08a54229c9ae4e92
Matsumoto Miu <miu.matsumoto04@gmail.comgit config --global user.email miu.matsumoto04@gmail.com>
2020-05-05 19:58:58 +0900

    feat(Work): complete work page

--- src/components/Work.js
+++ src/components/Work.js
@@ -13,13 +13,15 @@ const useStyles = makeStyles({
   root: {
     maxWidth: 345,
     margin: 'auto',
-    marginTop: '5rem',
+    marginTop: '2rem',
   },
   media: {
     height: 160,
   },
   button: {
-    width: 200,
+    width: 330,
+    display: 'block',
+    textAlign: 'center',
   },
 });
 
@@ -27,72 +29,86 @@ export default function Work() {
   const classes = useStyles();
 
   return (
-    <div className="work-container">
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image={work}
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="h2"
-              style={{ color: '#333' }}
+    <div>
+      <div className="container">
+        <h1>Work</h1>
+      </div>
+      <div className="work-container">
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={work}
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="h2"
+                style={{ color: '#333' }}
+              >
+                Task Manager
+              </Typography>
+              <Typography
+                variant="body2"
+                component="p"
+                style={{ color: '#333' }}
+              >
+                タスクを管理してくれるアプリです。
+                <br />
+                <br />
+                使用した技術: ReactHooks
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions className={classes.button}>
+            <a
+              href="https://github.com/matsumoto-parker"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              Task Manager
-            </Typography>
-            <Typography variant="body2" component="p" style={{ color: '#333' }}>
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions className={classes.button}>
-          <Button
-            size="large"
-            style={{ backgroundColor: '#3a8daf', width: 150 }}
-          >
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
-      </Card>
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image={work}
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="h2"
-              style={{ color: '#333' }}
-            >
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="" component="p">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
-      </Card>
+              <Button
+                size="large"
+                style={{ backgroundColor: '#3a8daf', width: 300 }}
+              >
+                見てみる
+              </Button>
+            </a>
+          </CardActions>
+        </Card>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={work}
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="h2"
+                style={{ color: '#333' }}
+              >
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="" component="p">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
     </div>
   );
 }
